da696965aeac38547137bddd91197f07baed2e1c
Andrei Ts <liptonbee@gmail.com>
2017-01-03 14:53:04 +0300

    page style

--- app/src/app.tpl.ts
+++ app/src/app.tpl.ts
@@ -1,12 +1,19 @@
 export const template = `
 <div class="rootcontainer">
   <my-header></my-header>
-    <div class="appcontainer">
-      <div *ngIf="enableChild">
-          <weather [amounttowns]="amountTowns" [location]="coordinates"></weather>     
-          <googlemap [zoom]="zoom" [location]="coordinates"></googlemap>
-      </div>
+  <div class="appcontainer">
+    <div *ngIf="!enableChild">   
+        <span>Waiting for location resolve.</span>
     </div>
+    <div *ngIf="enableChild">
+        <div class="weatherbox">
+            <weather [amounttowns]="amountTowns" [location]="coordinates"></weather>
+        </div>
+        <div class="googlemapbox">
+            <googlemap [zoom]="zoom" [location]="coordinates"></googlemap>     
+        </div>        
+    </div>
+  </div>
   <my-footer></my-footer>
 </div>
 `;
