470a98618683c41b516f8f85ffded982f5446c97
Andrei.Tsitou <andrei_tsitou@epam.com>
2017-01-05 19:14:15 +0300

    date update - temp 02

--- app/src/components/header/header.tpl.ts
+++ app/src/components/header/header.tpl.ts
@@ -1,7 +1,7 @@
 export const template = `
 <div class="header">
     <div class="appcontainer">
-        <span>Last update: </span><span>{{lastUpddateTime| date}}</span>
+        <span>Last update: </span><span>{{lastUpddateTime| date:'medium'}}</span>
     </div>    
 </div>
 `;
